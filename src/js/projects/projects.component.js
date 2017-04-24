6caba9fcee7b8e0b707daf41d38763b63484708e
Carlos P Navarrete Filho <karnav@gmail.com>
2017-04-24 20:15:28 -0300

    Initial Css for projects module.

--- src/js/projects/projects.component.js
+++ src/js/projects/projects.component.js
@@ -11,7 +11,7 @@ angular.module('projects')
             title: 'Neighborhood Map',
             imgSrc: 'dist/images/projects/neighborhoodMap.jpg',
             imgAlt: 'Image of the project: ' + this.title + '.',
-            subtitle: 'A map of central Edinburgh with tourist information on select points of interest.', 
+            subtitle: 'A map of central Edinburgh with tourist information on select points of interest:', 
             bulletpoints: [
               'Users can browse a list of points of interest, filter them, and get Wikipedia information on the ones they are interested in.',
               'Built in Javascript using Google Maps/Wikipedia(Media Wiki) APIs and KnockoutJS.',
@@ -27,7 +27,7 @@ angular.module('projects')
             title: 'Arcade Game',
             imgSrc: 'dist/images/projects/arcadeGame.jpg',
             imgAlt: 'Image of the project: ' + this.title + '.',
-            subtitle: 'A version of Frogger, an old game from the 80s where the goal was to cross a street under heavy traffic.', 
+            subtitle: 'A version of Frogger, an old game from the 80s where the goal was to cross a street under heavy traffic:', 
             bulletpoints: [
               'The player must take the character across the street, moving one tile at a time and avoiding the bugs.',
               'Built with pure Javascript and focused on best practices and Object-Oriented design.',
@@ -43,7 +43,7 @@ angular.module('projects')
             title: 'This Website',
             imgSrc: 'dist/images/projects/portfolioWebsite.jpg',
             imgAlt: 'Image of the project: ' + this.title + '.',
-            subtitle: 'My Portfolio Website. Intended to be an alternative, interactive version of my resume.', 
+            subtitle: 'My Portfolio Website. Intended to be an alternative, interactive version of my resume:', 
             bulletpoints: [
               'Visitors can scroll down the page and browse through some interactive sections of my resume.',
               'Built with JavaScript ES5, AngularJS1, HTML5/CSS3 and Gulp for Image Optimization/Minification.',
