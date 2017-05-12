459c689e6a2a4595a34cab898327ba7b192a2373
Carlos P Navarrete Filho <karnav@gmail.com>
2017-05-12 15:58:23 -0300

    Add skills module Css.

--- src/js/skills/skills.component.js
+++ src/js/skills/skills.component.js
@@ -5,20 +5,62 @@ angular.module('skills')
     templateUrl: 'dist/js/skills/skills.template.html',
     controller: function SkillsController(){
       this.skills = [
-        'AngularJS',
-        'JavaScript',
-        'WebApi',
-        'Entity Framework',
-        'HTML',
-        'Gulp',
-        'Git',
-        'C#',
-        'T-Sql',
-        'CSS',
-        'ASP.NET MVC',
-        'JasmineJS',
-        'Npm',
-        'Scrum'
+        { 
+          name: 'AngularJS',
+          size: { 'font-size' : '60px' }
+        },
+        {
+          name: 'JavaScript',
+          size: { 'font-size' : '60px' }
+        },
+        {
+          name: 'WebApi',
+          size: { 'font-size' : '10px' }
+        },
+        {
+          name: 'Entity Framework',
+          size: { 'font-size' : '10px' }
+        },
+        {
+          name: 'HTML',
+          size: { 'font-size' : '40px' }
+        },
+        {
+          name: 'Gulp',
+          size: { 'font-size' : '30px' }
+        },
+        {
+          name: 'Git',
+          size: { 'font-size' : '40px' }
+        },
+        {
+          name: 'C#',
+          size: { 'font-size' : '10px' }
+        },
+        {
+          name: 'T-Sql',
+          size: { 'font-size' : '10px' }
+        },
+        {
+          name: 'CSS',
+          size: { 'font-size' : '40px' }
+        },
+        {
+          name: 'ASP.NET MVC',
+          size: { 'font-size' : '20px' }
+        },
+        {
+          name: 'JasmineJS',
+          size: { 'font-size' : '30px' }
+        },
+        {
+          name: 'Npm',
+          size: { 'font-size' : '20px' }
+        },
+        {
+          name: 'Scrum',
+          size: { 'font-size' : '20px' }
+        }
       ]
     }
   });
