ce16cd0bda0844e0e2fd76cc3cada88a0f679c65
Carlos P Navarrete Filho <karnav@gmail.com>
2018-11-14 14:14:31 -0200

    remove unnecessary css and files to start blog

--- src/Blog.js
+++ src/Blog.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+class Blog extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
@@ -25,4 +21,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Blog;
