c9820a6ed4c2b68f4e1e2ca7712140d9a9a843f2
Carlos P Navarrete Filho <karnav@gmail.com>
2018-11-27 17:53:10 -0200

    create base component for intro text

--- src/Router.js
+++ src/Router.js
@@ -3,11 +3,13 @@ import { Route } from 'react-router-dom'
 
 import Background from './components/background'
 import ArticleList from './components/blog/articleList/container'
+import IntroText from './components/blog/introText'
 
 class Router extends Component {
   render () {
     return (
       <Background>
+        <IntroText />
         <Route exact path='/' component={ArticleList} />
       </Background>
     )
---
+++ src/components/blog/introText/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IntroText = () => (
+  <div>
+    <h1>This is a mock blog application built to test new techs.</h1>
+    <h4>By Carlos Navarrete</h4>
+    <p>I created this website to practice new technologies. The current goal is to use React, Redux, Redux-Saga, React-Router, Styled-Components, NodeJs, Express, Mongoose and MongoDB to build the application. Pick one of the articles to read the details and to be able to create comments for it.</p>
+  </div>
+)
+
+export default IntroText
